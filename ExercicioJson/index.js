de4d0f7a7692824ec1ef7afcf7b8a5f62713bb84
unknown <l.alves97@hotmail.com>
2018-11-11 18:39:29 +0000

    new features

--- ExercicioJson/index.js
+++ ExercicioJson/index.js
@@ -35,13 +35,53 @@ function createCartoon(section){
     var description = document.createElement("p");
     var charactersList = document.createElement("ul");
 
+    //put context inside type
+    var type = document.createElement("p");
+    type.innerHTML = initialState.section.type;
+    //put context inside title
+    title.innerHTML = initialState.sections.title;
+    //put text inside description
+    description.innerHTML = initialState.section.description;
+
     //ciclo para enquanto existir characters adicionar nova li com nome e gender
     for(j = 0; characters.length; j++ ){
         var character = document.createElement("li");
         charactersList.appendChild(character);
-        j++;
+        name = document.createElement("strong");
+        gender = document.createElement("strong");
+        character.appendChild(name);
+        character.appendChild(gender);
+        name.innerHTML = initialState.section.characters[j].name;
+        gender.innerHTML = initialState.section.characters[j].gender;
     }    
 }
 
+//make changes for data ** TODO
+function createData(section){
+    var j;
+    var type = document.createElement("p");
+    var title = document.createElement("h2");
+    var description = document.createElement("p");
+    var charactersList = document.createElement("ul");
 
+    //put context inside type
+    var type = document.createElement("p");
+    type.innerHTML = initialState.section.type;
+    //put context inside title
+    title.innerHTML = initialState.section.title;
+    //put text inside description
+    description.innerHTML = initialState.section.description;
+
+    //ciclo para enquanto existir characters adicionar nova li com nome e gender
+    for(j = 0; characters.length; j++ ){
+        var character = document.createElement("li");
+        charactersList.appendChild(character);
+        name = document.createElement("strong");
+        gender = document.createElement("strong");
+        character.appendChild(name);
+        character.appendChild(gender);
+        name.innerHTML = initialState.section.characters[j].name;
+        gender.innerHTML = initialState.section.characters[j].gender;
+    }    
+}
 
